91401d7801f95c87fa2d53b62517ec2fc5eadbcd
LiaoBoyu <57322822+Liao-BoYu@users.noreply.github.com>
2019-11-10 14:00:20 +0800

    add 2-6

---
+++ exercises/2-6.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int abc, def, ghi, count = 0;
+    for(abc = 123; abc <= 329; abc++)
+    {
+        int a = abc/100;
+        int b = abc/10%10;
+        int c = abc%10%10;
+        if(a != b && a != c && b != c)
+            for(def = 246; def <= 494; def++)
+            {
+                int d = def/100;
+                int e = def/10%10;
+                int f = def%10%10;
+                if(d != e && d != f && e != f)
+                    for(ghi = 369; ghi <= 987; ghi++)
+                    {
+                        int g = ghi/100;
+                        int h = ghi/10%10;
+                        int i = ghi%10%10;
+                        if(g != h && g != i && h != i)
+                            if(ghi == 3*abc && def == 2*abc)
+                                printf("Case %d: %d %d %d\n", ++count, abc, def, ghi);
+                    }
+            }
+    }
+    return 0;
+}
